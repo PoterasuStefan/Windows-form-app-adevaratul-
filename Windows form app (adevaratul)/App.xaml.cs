d211871a6518e675701d11232739cf9550eeae70
stefan poterasu <spoterasu@gmail.com>
2022-05-01 18:33:03 +0300

    Add project files.

---
+++ Windows form app (adevaratul)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Windows_form_app__adevaratul_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
